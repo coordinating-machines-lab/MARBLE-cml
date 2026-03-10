dad7f005d00f10a537934607fd501993e120af4e
Varun Gangal <varun.gangal@patronus.ai>
2026-03-10 19:43:22 -0400

    fix: update deps, README, and evaluation script for Mac/Unix

--- scripts/werewolf/run_evaluation.sh
+++ scripts/werewolf/run_evaluation.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python marble\\evaluator\\werewolf_evaluator.py --top_level_dir "werewolf_log" --config_path "marble\configs\test_config\werewolf_config\werewolf_config.yaml" --snapshot_folder "placeholder" --base_log_dir "path/to/your/output/dir"
+python marble/evaluator/werewolf_evaluator.py \
+  --top_level_dir "werewolf_log" \
+  --config_path "marble/configs/test_config/werewolf_config/werewolf_config.yaml" \
+  --snapshot_folder "evaluation_run_1" \
+  --base_log_dir "werewolf_log/evaluations"
